2512c686704e4f47f3724c89dffce0f0b1d77394
donna7 <donna@letsplax.com>
2023-06-09 12:10:11 +0900

    [example] image classification

--- lib/classifier.dart
+++ lib/classifier.dart
@@ -1,87 +1,127 @@
-import 'package:flutter/services.dart';
+import 'dart:math';
 
-// Import tflite_flutter
+import 'package:image/image.dart';
+import 'package:collection/collection.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
-class Classifier {
-  // name of the model file
-  final _modelFile = 'text_classification.tflite';
-  final _vocabFile = 'text_classification_vocab.txt';
+abstract class Classifier {
+  late Interpreter interpreter;
+  late InterpreterOptions _interpreterOptions;
 
-  // Maximum length of sentence
-  final int _sentenceLen = 256;
+  late List<int> _inputShape;
+  late List<int> _outputShape;
 
-  final String start = '<START>';
-  final String pad = '<PAD>';
-  final String unk = '<UNKNOWN>';
+  late TensorImage _inputImage;
+  late TensorBuffer _outputBuffer;
 
-  late Map<String, int> _dict;
+  late TfLiteType _inputType;
+  late TfLiteType _outputType;
 
-  // TensorFlow Lite Interpreter object
-  late Interpreter _interpreter;
+  final String _labelsFileName = 'assets/labels.txt';
 
-  Classifier() {
-    // Load model when the classifier is initialized.
-    _loadModel();
-    _loadDictionary();
-  }
+  final int _labelsLength = 1001;
 
-  void _loadModel() async {
-    // Creating the interpreter using Interpreter.fromAsset
-    _interpreter = await Interpreter.fromAsset(_modelFile);
-    print('Interpreter loaded successfully');
-  }
+  late var _probabilityProcessor;
 
-  void _loadDictionary() async {
-    final vocab = await rootBundle.loadString('assets/$_vocabFile');
-    var dict = <String, int>{};
-    final vocabList = vocab.split('\n');
-    for (var i = 0; i < vocabList.length; i++) {
-      var entry = vocabList[i].trim().split(' ');
-      dict[entry[0]] = int.parse(entry[1]);
-    }
-    _dict = dict;
-    print('Dictionary loaded successfully');
-  }
+  late List<String> labels;
 
-  List<double> classify(String rawText) {
-    // tokenizeInputText returns List<List<double>>
-    // of shape [1, 256].
-    List<List<double>> input = tokenizeInputText(rawText);
+  String get modelName;
 
-    // output of shape [1,2].
-    var output = List<double>.filled(2, 0).reshape([1, 2]);
+  NormalizeOp get preProcessNormalizeOp;
+  NormalizeOp get postProcessNormalizeOp;
 
-    // The run method will run inference and
-    // store the resulting values in output.
-    _interpreter.run(input, output);
+  Classifier({int? numThreads}) {
+    _interpreterOptions = InterpreterOptions();
 
-    return [output[0][0], output[0][1]];
-  }
-
-  List<List<double>> tokenizeInputText(String text) {
-    // Whitespace tokenization
-    final toks = text.split(' ');
-
-    // Create a list of length==_sentenceLen filled with the value <pad>
-    var vec = List<double>.filled(_sentenceLen, _dict[pad]!.toDouble());
-
-    var index = 0;
-    if (_dict.containsKey(start)) {
-      vec[index++] = _dict[start]!.toDouble();
+    if (numThreads != null) {
+      _interpreterOptions.threads = numThreads;
     }
 
-    // For each word in sentence find corresponding index in dict
-    for (var tok in toks) {
-      if (index > _sentenceLen) {
-        break;
-      }
-      vec[index++] = _dict.containsKey(tok)
-          ? _dict[tok]!.toDouble()
-          : _dict[unk]!.toDouble();
-    }
+    loadModel();
+    loadLabels();
+  }
 
-    // returning List<List<double>> as our interpreter input tensor expects the shape, [1,256]
-    return [vec];
+  Future<void> loadModel() async {
+    try {
+      interpreter =
+          await Interpreter.fromAsset(modelName, options: _interpreterOptions);
+      print('Interpreter Created Successfully');
+
+      _inputShape = interpreter.getInputTensor(0).shape;
+      _outputShape = interpreter.getOutputTensor(0).shape;
+      _inputType = interpreter.getInputTensor(0).type;
+      _outputType = interpreter.getOutputTensor(0).type;
+
+      _outputBuffer = TensorBuffer.createFixedSize(_outputShape, _outputType);
+      _probabilityProcessor =
+          TensorProcessorBuilder().add(postProcessNormalizeOp).build();
+    } catch (e) {
+      print('Unable to create interpreter, Caught Exception: ${e.toString()}');
+    }
+  }
+
+  Future<void> loadLabels() async {
+    labels = await FileUtil.loadLabels(_labelsFileName);
+    if (labels.length == _labelsLength) {
+      print('Labels loaded successfully');
+    } else {
+      print('Unable to load labels');
+    }
+  }
+
+  TensorImage _preProcess() {
+    int cropSize = min(_inputImage.height, _inputImage.width);
+    return ImageProcessorBuilder()
+        .add(ResizeWithCropOrPadOp(cropSize, cropSize))
+        .add(ResizeOp(
+            _inputShape[1], _inputShape[2], ResizeMethod.NEAREST_NEIGHBOUR))
+        .add(preProcessNormalizeOp)
+        .build()
+        .process(_inputImage);
+  }
+
+  Category predict(Image image) {
+    final pres = DateTime.now().millisecondsSinceEpoch;
+    _inputImage = TensorImage(_inputType);
+    _inputImage.loadImage(image);
+    _inputImage = _preProcess();
+    final pre = DateTime.now().millisecondsSinceEpoch - pres;
+
+    print('Time to load image: $pre ms');
+
+    final runs = DateTime.now().millisecondsSinceEpoch;
+    interpreter.run(_inputImage.buffer, _outputBuffer.getBuffer());
+    final run = DateTime.now().millisecondsSinceEpoch - runs;
+
+    print('Time to run inference: $run ms');
+
+    Map<String, double> labeledProb = TensorLabel.fromList(
+            labels, _probabilityProcessor.process(_outputBuffer))
+        .getMapWithFloatValue();
+    final pred = getTopProbability(labeledProb);
+
+    return Category(pred.key, pred.value);
+  }
+
+  void close() {
+    interpreter.close();
+  }
+}
+
+MapEntry<String, double> getTopProbability(Map<String, double> labeledProb) {
+  var pq = PriorityQueue<MapEntry<String, double>>(compare);
+  pq.addAll(labeledProb.entries);
+
+  return pq.first;
+}
+
+int compare(MapEntry<String, double> e1, MapEntry<String, double> e2) {
+  if (e1.value > e2.value) {
+    return -1;
+  } else if (e1.value == e2.value) {
+    return 0;
+  } else {
+    return 1;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,118 @@
+import 'dart:io';
+import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import './classifier.dart';
+import './classifier_quant.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late TextEditingController _controller;
-  late Classifier _classifier;
-  late List<Widget> _children;
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-    _classifier = Classifier();
-    _children = [];
-    _children.add(Container());
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.orangeAccent,
-          title: const Text('Text classification'),
-        ),
-        body: Container(
-          padding: const EdgeInsets.all(4),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                  child: ListView.builder(
-                itemCount: _children.length,
-                itemBuilder: (_, index) {
-                  return _children[index];
-                },
-              )),
-              Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.orangeAccent)),
-                child: Row(children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      decoration: const InputDecoration(
-                          hintText: 'Write some text here'),
-                      controller: _controller,
+      title: 'Image Classification',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Classifier _classifier;
+
+  File? _image;
+  final picker = ImagePicker();
+
+  Image? _imageWidget;
+
+  img.Image? fox;
+
+  Category? category;
+
+  @override
+  void initState() {
+    super.initState();
+    _classifier = ClassifierQuant();
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = File(pickedFile!.path);
+      _imageWidget = Image.file(_image!);
+
+      _predict();
+    });
+  }
+
+  void _predict() async {
+    img.Image imageInput = img.decodeImage(_image!.readAsBytesSync())!;
+    var pred = _classifier.predict(imageInput);
+
+    setState(() {
+      category = pred;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TfLite Flutter Helper',
+            style: TextStyle(color: Colors.white)),
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: _image == null
+                ? const Text('No image selected.')
+                : Container(
+                    constraints: BoxConstraints(
+                        maxHeight: MediaQuery.of(context).size.height / 2),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
                     ),
+                    child: _imageWidget,
                   ),
-                  TextButton(
-                    child: const Text('Classify'),
-                    onPressed: () {
-                      final text = _controller.text;
-                      final prediction = _classifier.classify(text);
-                      setState(() {
-                        _children.add(Dismissible(
-                          key: GlobalKey(),
-                          onDismissed: (direction) {},
-                          child: Card(
-                            child: Container(
-                              padding: const EdgeInsets.all(16),
-                              color: prediction[1] > prediction[0]
-                                  ? Colors.lightGreen
-                                  : Colors.redAccent,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    "Input: $text",
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                  Text("Output:"),
-                                  Text("   Positive: ${prediction[1]}"),
-                                  Text("   Negative: ${prediction[0]}"),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ));
-                        _controller.clear();
-                      });
-                    },
-                  ),
-                ]),
-              ),
-            ],
           ),
-        ),
+          const SizedBox(
+            height: 36,
+          ),
+          Text(
+            category != null ? category!.label : '',
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            category != null
+                ? 'Confidence: ${category!.score.toStringAsFixed(3)}'
+                : '',
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: const Icon(Icons.add_a_photo),
       ),
     );
   }
